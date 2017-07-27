f7aeab3892f6adc5ac4175aec4d74086a20869f9
Cindy Salinas <cindysalinas15@gmail.com>
2017-07-27 15:53:12 -0400

    remove sections load

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-  // load header
-  $('#header').load('../src/templates/header.html');
-  // load footer
-  $('#footer').load('../src/templates/footer.html');
-  // load content
-  $('#content').load('../src/templates/content.html');
+
 });
