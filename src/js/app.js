439974ab9142c63506b710083a2d752e47a177ab
Cindy Salinas <cindysalinas15@gmail.com>
2017-07-27 14:55:39 -0400

    load page sections

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  // load header
+  $('#header').load('../src/templates/header.html');
+  // load footer
+  $('#footer').load('../src/templates/footer.html');
+  // load content
+  $('#content').load('../src/templates/content.html');
+});
